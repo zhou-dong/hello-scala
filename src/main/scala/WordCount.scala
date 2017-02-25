931d8a6835b172220d17a688ad7c33f5fbd5eadd
zhou-dong <82224165@qq.com>
2017-02-25 03:55:25 -0600

    add spark sql context

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,25 +1,36 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
 
 object WordCount {
 
   def main(args: Array[String]) {
 
+    var minPartitions = 5;
+
     val conf = new SparkConf().setAppName("work-count").setMaster("local")
     val sc = new SparkContext(conf)
 
     val assetsPath = "file:///Users/zhoudong/Downloads/assets_2014-01-20_00_domU-12-31-39-01-A1-34"
-    val lines = sc.textFile(assetsPath)
-    val lineLengths = lines.map(s => s.length)
+    val adEventsPath = "file:///Users/zhoudong/Downloads/ad-events_2014-01-20_00_domU-12-31-39-01-A1-34"
+
+    val assetLines = sc.textFile(assetsPath, minPartitions)
+    val adLines = sc.textFile(adEventsPath, minPartitions)
+
+    SQLContext
+    
+    val lineLengths = assetLines.map(s => s.length)
     val totalLength = lineLengths.reduce((a, b) => a + b)
 
     println(lineLengths)
     println(totalLength)
 
-    val numAs = lines.filter(line => line.contains("a")).count()
-    val numBs = lines.filter(line => line.contains("z")).count()
+    val numAs = assetLines.filter(line => line.contains("a")).count()
+    val numBs = assetLines.filter(line => line.contains("z")).count()
     println(numAs)
     println(numBs)
+    
+    assetLines.filter(line => line.contains("a"));
   }
 
 }
